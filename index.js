35a687dcb5c5f30ef3d3acf4dc13b9d1b05f6807
jtx007 <jamesjacobthomas7@gmail.com>
2020-07-10 21:49:55 -0400

    popover element without transition

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // TODO: Implement JS and addEventListener to share svg button to show popover on click
 const shareButton = document.querySelector('.share-svg-button')
-console.log(shareButton)
+const popover = document.querySelector('.popover')
+
+shareButton.addEventListener('click', () => {
+    if (popover.classList.contains('show')) {
+        popover.classList.remove('show')
+    } else {
+        popover.classList.add('show')
+    }
+})
