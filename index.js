6b88139071790f3e5a5d47fbc9f2d0a0b220cd89
jtx007 <jamesjacobthomas7@gmail.com>
2020-07-13 19:42:18 -0400

    MVP of article component complete

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 const shareButton = document.querySelector('.share-svg-button')
 const popover = document.querySelector('.popover')
 
+
+
+
 shareButton.addEventListener('click', () => {
+    event.preventDefault();
     if (popover.classList.contains('show')) {
         popover.classList.remove('show')
     } else {
         popover.classList.add('show')
+    }
+})
+
+shareButton.addEventListener('focusout', (event) => {
+    event.preventDefault()
+    if (popover.classList.contains("show")) {
+      popover.classList.remove("show");
+    } else {
+      popover.classList.add("show");
     }
 })
